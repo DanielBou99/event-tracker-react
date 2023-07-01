7672b6cd65d20a63545f3ea74ba6aaff1f7c111f
Daniel Bou de Souza <danielbou@outlook.com.br>
2023-06-30 22:23:24 -0300

    Remover evento recoil

--- src/App.tsx
+++ src/App.tsx
@@ -27,22 +27,12 @@ function App() {
 
   const [filtro, setFiltro] = useState<Date | null>();
 
-  const adicionarEvento = (evento: IEvento) => {
-    evento.id = Math.round(new Date().getTime() / 1000);
-    // eventos.push(evento)
-    // console.log(eventos);
-
-    // setEventos([...eventos])
-  };
   const alterarStatusEvento = (id: number) => {
     // const evento = eventos.find(evento => evento.id === id)
     // if (evento) {
     //   evento.completo = !evento.completo
     // }
     // setEventos([...eventos])
-  };
-  const deletarEvento = (id: number) => {
-    // setEventos([...eventos.filter(evento => evento.id !== id)])
   };
 
   const aplicarFiltro = (data: Date | null) => {
@@ -67,7 +57,6 @@ function App() {
             <ListaDeEventos
               aoFiltroAplicado={aplicarFiltro}
               aoAlterarStatus={alterarStatusEvento}
-              aoDeletarEvento={deletarEvento}
             />
           </Card>
         </div>
